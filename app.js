7eaa3b12efb56c0cbdb1e6bcb1b00f5047ed71c6
Alex Martin <martin.alex.t@gmail.com>
2014-10-17 01:43:53 +0000

    Add scaffolding for initial menu. I created the startup menu for the app and put a bit of flow in place to demonstrate Inquirer's prompts. The only "working" bit at the moment is the "Setup" option, which I hope to flesh out soon. There are more requires than I need right now but they should come into play soon.

--- app.js
+++ app.js
@@ -7,21 +7,68 @@ var shell = require('shelljs'),
 
 Array.prototype.includes = function(v) { return this.indexOf(v) !== -1; } 
 
-
-console.log("Welcome to Node-Buster!");
-
+//---------------------QUESTIONS---------------------------
 var questionsInit = [
   {
     type: "list",
-    name: "Command",
+    name: "startup",
     message: "What would you like to do?",
     choices: ["1. Set up a local repo", "2. Configure an existing repo", "3. Generate static pages", "4. Push existing pages to my repo"],
     filter: function (choice) { return choice.charAt(0); }
     }
 ];
 
-inquirer.prompt( questionsInit, function(answers) {
-  console.log(JSON.stringify(answers, null, ' '));
-});
+var questionsSetup = [
+  {
+      type: "confirm",
+      name: "confirmation",
+      message: "This will delete any existing content. Is this okay?",
+      default: false
+  }
+]
+
+//------------------PROMPTS--------------------------------
+var startup = function() { 
+  inquirer.prompt( questionsInit, function(answers) {
+    switch (answers.startup) {
+      case "1":
+        promptSetup();  
+        break;
+      case "2":
+        promptConfigure();
+        break;
+      case "3":
+        promptGenerate();
+        break;
+      case "4":
+        promptPush();
+        break;
+      default:
+        console.log("Sorry, I didn't understand that!");
+    } 
+  });
+}
+
+var promptSetup = function() {
+  console.log("This will create a 'static' folder in the current directory and set up an empty git repo within it.");
+  inquirer.prompt(questionsSetup, function(answers) {
+    if (answers.confirmation) {
+      console.log("Beginning git setup!");
+    } else {
+      console.log("Returning to index...");
+      startup();
+    }
+  }); 
+};
+var promptConfigure = function() {};
+var promptGenerate = function() {};
+var promptPush = function() {};
 
 
+//-----------------METHODS-------------------------------
+
+
+
+//--------------PROGRAM FLOW---------------------------
+console.log("Welcome to Node-Buster!");
+startup();
