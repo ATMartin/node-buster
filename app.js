20f8bf5b36449081a3fc440b9c220ec8c4714cc0
Alex Martin <martin.alex.t@gmail.com>
2014-10-17 03:11:24 +0000

    Add generate, setup and quit methods.
    I added methods for setting up Git and generating static pages.
    I also refactored some of the core functionality so the program now acts as a loop, instead
    of a one-off task runner.
    WARNING: exec in shelljs is known to be a CPU hog. Be aware when using it.

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var questionsInit = [
     type: "list",
     name: "startup",
     message: "What would you like to do?",
-    choices: ["1. Set up a local repo", "2. Configure an existing repo", "3. Generate static pages", "4. Push existing pages to my repo"],
+    choices: ["1. Set up a local repo", "2. Configure an existing repo", "3. Generate static pages", "4. Push existing pages to my repo", "5. Quit this utility."],
     filter: function (choice) { return choice.charAt(0); }
     }
 ];
@@ -25,7 +25,15 @@ var questionsSetup = [
       message: "This will delete any existing content. Is this okay?",
       default: false
   }
-]
+];
+
+var questionsGenerate = [
+  {
+    type: "input",
+    name: "sourceUrl",
+    message: "Where is your blog currently located?"
+  }
+];
 
 //------------------PROMPTS--------------------------------
 var startup = function() { 
@@ -43,6 +51,9 @@ var startup = function() {
       case "4":
         promptPush();
         break;
+      case "5":
+        promptExit();
+        break;
       default:
         console.log("Sorry, I didn't understand that!");
     } 
@@ -54,19 +65,50 @@ var promptSetup = function() {
   inquirer.prompt(questionsSetup, function(answers) {
     if (answers.confirmation) {
       console.log("Beginning git setup!");
+      shell.rm("-rf", "./static");
+      shell.mkdir("static");
+      shell.cd("static");
+      shell.exec("git init");
+      shell.exec("git checkout -b 'gh-pages'");
+      shell.cd("..");
+      console.log("Git setup complete!");
     } else {
       console.log("Returning to index...");
-      startup();
     }
+    startup();
   }); 
 };
 var promptConfigure = function() {};
-var promptGenerate = function() {};
-var promptPush = function() {};
-
-
-//-----------------METHODS-------------------------------
-
+var promptGenerate = function() {
+  inquirer.prompt(questionsGenerate, function(answers) {
+    var commandText = "wget --recursive" +
+                      " --convert-links" +
+                      " --page-requisites" +
+                      " --no-parent" +
+                      " --directory-prefix ./static" +
+                      " --no-host-directories" +
+                      " " + answers.sourceUrl;
+    console.log("Retreiving your blog from " + answers.sourceUrl + " now!");
+    shell.exec(commandText, function(err, stdout, stderr) {
+      if (err === null || err === "0") { 
+        console.log("Static generation complete!"); 
+      } else { 
+        console.log("Uh-oh, something broke: " + err);   
+      }
+      startup();
+    });
+  });
+};
+var promptPush = function() {
+  shell.cd("static");
+  shell.exec("git push origin gh-pages");
+  shell.cd("..");
+  startup();
+};
+var promptExit = function() {
+  console.log("Thanks for using Node-Buster! See you soon!");
+  process.exit(0);
+};
 
 
 //--------------PROGRAM FLOW---------------------------
